0925c13c6993cee79225581ff50057985a88b873
Johann Carlos <90796061+Johanncf@users.noreply.github.com>
2021-11-06 18:06:46 -0300

    Código finalizado

--- ExerciciosSemana3/Ex5/script.js
+++ ExerciciosSemana3/Ex5/script.js
@@ -1,15 +1,17 @@
-
-
-function carrega(){
-    var lampStatus = document.getElementById('lamp')
+var acesa = false;
+var lampStatus;
+document.addEventListener("DOMContentLoaded", () => {
+    lampStatus = document.getElementById('lamp')
     lampStatus.src = "https://raw.githubusercontent.com/phmonte/devinhouse/main/Aulas/Javascript/imagens/luz-apagada.png"
     acesa = false;
-}
+})
 
 function ligaDesliga(){
     if(!acesa){
         lampStatus.src = "https://raw.githubusercontent.com/phmonte/devinhouse/main/Aulas/Javascript/imagens/luz-acesa.png"
+        acesa = true;
     }else{
         lampStatus.src = "https://raw.githubusercontent.com/phmonte/devinhouse/main/Aulas/Javascript/imagens/luz-apagada.png"
+        acesa = false;
     }
 }
